c9a1d143edc87ac2bc36717f1cea853fda36bae1
zigotero <jashirchirre@gmail.com>
2019-08-01 22:06:12 -0500

    creando base de registro para mvp

---
+++ Proyectoadm/app/src/main/java/com/example/proyectoadm/presentation/register/view/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyectoadm.presentation.register.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyectoadm.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
