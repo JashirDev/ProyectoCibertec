197c104fb41ad8e786b3c61d6cacfa213574fbf3
zigotero <jashirchirre@gmail.com>
2019-08-04 03:07:30 -0500

    maquetado hasta navigation

--- Proyectoadm/app/src/main/java/com/example/proyectoadm/presentation/main/view/MainActivity.java
+++ Proyectoadm/app/src/main/java/com/example/proyectoadm/presentation/main/view/MainActivity.java
@@ -2,15 +2,48 @@ package com.example.proyectoadm.presentation.main.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.proyectoadm.R;
+import com.example.proyectoadm.presentation.navigationdrawer.view.NavigationActivity;
+import com.example.proyectoadm.presentation.register.view.RegisterActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private Button btLogin;
+    private TextInputEditText tiEmail, tiPassword;
+    private TextView tvGoToRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btLogin = findViewById(R.id.btLogin);
+        tiEmail= findViewById(R.id.tiEmail);
+        tiPassword= findViewById(R.id.tiPassword);
+        tvGoToRegister=findViewById(R.id.tvGoRegister);
+
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NavigationActivity.class);
+                startActivity(i);
+            }
+        });
+
+        tvGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- Proyectoadm/app/src/main/java/com/example/proyectoadm/presentation/register/view/RegisterActivity.java
+++ Proyectoadm/app/src/main/java/com/example/proyectoadm/presentation/register/view/RegisterActivity.java
@@ -2,15 +2,47 @@ package com.example.proyectoadm.presentation.register.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.proyectoadm.R;
+import com.example.proyectoadm.presentation.navigationdrawer.view.NavigationActivity;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputEditText registerEmail,registerName,registerPassword,registerConfirmPassword;
+    private Button btRegister, btCancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerEmail=findViewById(R.id.tvRegisterEmail);
+        registerName=findViewById(R.id.tvRegisterName);
+        registerPassword=findViewById(R.id.tvRegisterPassoword);
+        registerConfirmPassword=findViewById(R.id.tvConfirmPassword);
+        btRegister=findViewById(R.id.btRegistrar);
+        btCancel=findViewById(R.id.btCancelar);
+
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(RegisterActivity.this, NavigationActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        btCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
